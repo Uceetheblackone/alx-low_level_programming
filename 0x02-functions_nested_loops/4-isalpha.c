879ad787114ded481bfe3b17535e6c1cea614e3c
Uceetheblackone <anyanwuuchechukwujaygmail.com>
2023-02-22 04:34:02 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - to check of char is a letter, lowercase or upper case
+ * @c: is the char to be checked
+ * Return: 1 if char is a letter, lowercase or uppercase, otherwise 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+			return (1);
+			else
+			return (0);
+}
