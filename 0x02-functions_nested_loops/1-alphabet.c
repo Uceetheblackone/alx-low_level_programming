ae9125ad195dd56690994772a1e623c7ce06fdfe
Uceetheblackone <anyanwuuchechukwujaygmail.com>
2023-02-22 02:37:35 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	i = 'a';
+	while
+		(i <= 'z');
+	_putchar(i);
+	i++;
+
+	_putchar('\n');
+}
