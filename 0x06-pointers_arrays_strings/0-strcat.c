37193caf1285c7abc720fa71fec01d859a1fbad8
Uceetheblackone <anyanwuuchechukwujaygmail.com>
2023-03-02 11:56:11 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+
+	int j;
 
 	i = 0;
 
 	while (dest[i] != '\0')
+	{
 		i++;
-	for (j = 0; src[j] != '\0'; j++)
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
 		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	return (dest);
 }
