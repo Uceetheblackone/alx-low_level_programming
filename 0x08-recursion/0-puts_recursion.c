60c4fe101591278e4e041d7ccf2079d633b314fc
Uceetheblackone <anyanwuuchechukwujaygmail.com>
2023-03-10 01:51:14 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function like puts();
+ * @s: input value
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
