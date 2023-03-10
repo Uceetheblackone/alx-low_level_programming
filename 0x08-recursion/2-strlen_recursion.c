f6141886b444daa7ca0d074d097f188d64600deb
Uceetheblackone <anyanwuuchechukwujaygmail.com>
2023-03-10 02:29:51 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- *_stren_recursion - Returns the length of a string.
+ *_strlen_recursion - Returns the length of a string.
  *@s: input value.
  *
  *Return: The length of the string.
  */
-int _stren_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	int l;
 
@@ -15,7 +15,7 @@ int _stren_recursion(char *s)
 	if (*s)
 	{
 		l++;
-		l = l + _stren_recursion(s + 1);
+		l = l + _strlen_recursion(s + 1);
 	}
 	return (l);
 }
