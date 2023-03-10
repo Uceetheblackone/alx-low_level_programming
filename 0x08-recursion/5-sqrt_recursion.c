1d6a616daf3672c8358a40df98bb04688909ac15
Uceetheblackone <anyanwuuchechukwujaygmail.com>
2023-03-10 03:33:40 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,11 +10,11 @@
  */
 int actual_sqrt_recursion(int n, int i)
 {
-        if (i * i > n)
-                return (-1);
-        if (i * i == n)
-                return (i);
-        return (actual_sqrt_recursion(n, i + 1));
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
 }
 /**
  * _sqrt_recursion - write a function that returns
