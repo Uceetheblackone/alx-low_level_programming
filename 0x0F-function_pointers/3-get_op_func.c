a01eced36a8bf8419288a7a05799378928e219c5
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-03-23 03:01:09 -0700

    0-print_name.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
-/*  *get_op_func - Selects the correct function to perform
+/**  *get_op_func - Selects the correct function to perform
 * the operation asked by the user.
 * @s: The operator passed as argument.
 *
