e8031634275d3fe6c4e4802262df2176b020cfb4
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-03-23 13:24:03 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,7 +4,7 @@
 
 /**
 * print_strings - Prints strings, followed by a new line.
-* @ *separator: The string to be printed between strings.
+* @separator: The string to be printed between strings.
 * @n: The number of strings passed to the function.
 * @...: A variable number of strings to be printed.
 *
