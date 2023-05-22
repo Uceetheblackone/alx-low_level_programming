1970ddc644e7f1a34ca3817409b589290516fd87
Uceetheblackone <anyanwuuchechukwujay@gmail.com>
2023-05-22 14:16:49 -0700

    100-operation.so

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+
+
+/**
+ * add - functions that finds the sum of two numbers.
+ * @a: input value
+ * @b: input value
+ * Return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - functions that finds the difference of two numbers.
+ * @a: input value
+ * @b: input value
+ * Return: the difference of a and b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - functions that finds the multiple of two numbers.
+ * @a: input value
+ * @b: input value
+ * Return: the multiple of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - functions that finds the division of two numbers.
+ * @a: input value
+ * @b: input value
+ * Return: the division of a and b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - functions that finds the modulus of two numbers.
+ * @a: input value
+ * @b: input value
+ * Return: the modulus of a and b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
